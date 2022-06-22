fe8e8b50442a1ecdcd259076ede9f09d39074666
LaupWing <loc_strict@hotmail.com>
2022-06-22 17:32:35 +0200

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+   mode: 'jit',
+   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+   theme: {
+      extend: {},
+   },
+   plugins: [],
 }
