6b20982451f28a35858d421bee5dd84a1c0482e4
LaupWing <loc_strict@hotmail.com>
2022-06-22 17:52:08 +0200

    updated

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,22 @@ export default function Home() {
       quantity: 0,
       price: 999,
    })
+
+   const changeQuantity = (value) => {
+      setItem({ ...item, quantity: Math.max(0, value) })
+   }
+    
+   const onQuantityPlus = () => {
+      changeQuantity(item.quantity + 1)
+   }
+    
+   const onQuantityMinus = () => {
+      changeQuantity(item.quantity - 1)
+   }
+   const onInputChange = (e) => {
+      changeQuantity(parseInt(e.target.value));
+   }
+
    return (
       <div className={styles.container}>
          <Head>
@@ -19,6 +35,42 @@ export default function Home() {
             <meta name="description" content="Complete Step by Step Tutorial" />
             <link rel="icon" href="/favicon.ico" />
          </Head>
+         <main>
+         <div className='shadow-lg border rounded p-2 '>
+            <Image src={item.image} width={300} height={150} alt={item.name} />
+            <h2 className='text-2xl'>$ {item.price}</h2>
+            <h3 className='text-xl'>{item.name}</h3>
+            <p className='text-gray-500'>{item.description}</p>
+            <p className='text-sm text-gray-600 mt-1'>Quantity:</p>
+            <div className='border rounded'>
+               <button
+                  className='bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-600'
+                  onClick={onQuantityMinus}
+                  >
+               -
+               </button>
+               <input
+                  type='number'
+                  className='p-2'
+                  value={item.quantity}
+                  onChange={onInputChange}
+               />
+               <button
+                  className='bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-600'
+                  onClick={onQuantityPlus}
+               >
+               +
+               </button>
+            </div>
+            <p>Total: ${item.quantity * item.price}</p>
+            <button
+               disabled={item.quantity === 0}
+               className='bg-blue-500 hover:bg-blue-600 text-white block w-full py-2 rounded mt-2 disabled:cursor-not-allowed disabled:bg-blue-100'
+            >
+               Buy
+            </button>
+         </div>
+         </main>
       </div>
    )
 }
