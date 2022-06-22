e8c1c4c602997c6db73f42aadb12b691b56ec103
LaupWing <loc_strict@hotmail.com>
2022-06-22 19:24:51 +0200

    CreateStripeSession

--- pages/api/create-stripe-session.js
+++ pages/api/create-stripe-session.js
@@ -1,7 +1,11 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
-export default async (req,res)=>{
+export default async function CreateStripeSession (req,res){
    const {item} = req.body
+
+   const redirectURL = process.env.NODE_ENV === 'development' ?
+      'http://localhost:3000' : 
+      'https://stripe-checkout-next-js-demo.vercel.app'
 
    const stripe_object = {
       price_data:{
@@ -15,4 +19,17 @@ export default async (req,res)=>{
       description: item.description,
       quantity: item.quantity
    }
+
+   const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [stripe_object],
+      mode: 'payment',
+      success_url: `${redirectURL}?status=success`,
+      cancel_url: `${redirectURL}?status=cancel`,
+      metadata:{
+         images: item.image
+      }
+   })
+
+   res.json({id:session.id})
 }
